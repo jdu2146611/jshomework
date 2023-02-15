9bc70a8f24c8825feee32ed9e0ec08814f95a498
jdu2146611 <rkmanreigns1212@gmail.com>
2023-02-15 21:03:29 +0500

    TV-Shop Done

--- main.js
+++ main.js
@@ -32,6 +32,25 @@ function ready(){
     var input = quantityInputs[i];
     input.addEventListener('change', quantityChanged);
   }
+  //Karta qo'shish
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++){
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+}
+
+//Buy tugmasi ishlatish
+document.getElementsByClassName("btn-buy")[0].addEventListener("click", buyButtonClicked);
+
+//Buy tugmasi funksiyasi
+function buyButtonClicked(){
+    alert("Your Order is placed");
+    var cartContent = document.getElementsByClassName("cart-content")[0]
+    while (cartContent.hasChildNodes()){
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
 }
   // kartani ochirish funksiyasi
 function removeCartItem(event){
@@ -47,11 +66,43 @@ function quantityChanged(event){
     } 
     updatetotal();
 }
+//Karta qo'shish
+function addCartClicked(event){
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+
+    updatetotal();
+}
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title) {
+        alert("You have already add this item to cart");
+        return;
+    }
+}
+var cartBoxContent = `
+                        <img src="${productImg}" alt="" class="cart-img">
+                        <div class="detail-box">
+                        <div class="cart-product-title">${title}</div>
+                        <div class="cart-price">${price}</div>
+                        <input type="number" value="1" class="cart-quantity">
+                        </div>
+                        <!-- O'chiradigan ikonka -->
+                        <i class='bx bxs-trash-alt cart-remove'></i>`;
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
+cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);}
 
 // Umumiy hisoblash
-
-
-
 function updatetotal() {
    var  cartContent = document.getElementsByClassName("cart-content")[0];
    var cartBoxes = cartContent.getElementsByClassName("cart-box");
@@ -63,9 +114,10 @@ function updatetotal() {
         var price = parseFloat(priceElement.innerText.replace("$",""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
-
+   }
+    total = Math.round(total *100) / 100;
         document.getElementsByClassName('total-price')[0].innerText = '$' +total;
     
-    }
+    
 
 }
